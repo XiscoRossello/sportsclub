518aedee3d2e8910343f3726462835ffcffc4165
Xisco <frossellojeronimo@cifpfbmoll.eu>
2026-01-13 17:06:38 +0100

    feat: modified settings.py to accept localhost

--- sportsclub/sportsclub/settings.py
+++ sportsclub/sportsclub/settings.py
@@ -157,3 +157,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 #     "https://sportsclub.com",
 #     "https://app.sportsclub.com",
 # ]
+
+# CSRF Trusted Origins
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
